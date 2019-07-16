c616438821da4838ec9f110136088971303fd171
Your Name <you@example.com>
2019-07-16 22:10:53 +0700

    view dan templating revisi

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -5,7 +5,7 @@ class About extends Controller{
         $data['nama'] = $nama;
         $data['pekerjaan'] = $pekerjaan;
         $data['umur'] = $umur;
-        $data['judul'] = 'About me'
+        $data['judul'] = 'About me';
 
         $this->view('templates/header', $data);
         $this->view('about/index', $data);
