5c7e5416d328d7a0b5a72045b93f709aec85a8d2
Your Name <you@example.com>
2019-07-16 22:09:47 +0700

    views and templating

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,11 +1,21 @@
 <?php
 
-class About{
+class About extends Controller{
     public function index($nama = 'Farhan', $pekerjaan = 'Programmer', $umur = 19){
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan. Saya berumur $umur tahun.";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About me'
+
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page(){
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,2 @@
+<h1>About Me</h1>
+<p>Halo, nama saya <?php echo $data['nama'] ?>. Pekerjaan saya adalah seorang <?php echo $data['pekerjaan'] ?>. Umur saya masih <?php echo $data['umur'] ?></p>
