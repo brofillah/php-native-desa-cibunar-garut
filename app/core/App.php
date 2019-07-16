57c054683be6bd0cdbdb40734d17cbfdef6e8495
Your Name <you@example.com>
2019-07-16 15:08:20 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,17 @@
 
 class App {
     public function __construct(){
-        echo "Okrayy!";
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+
+    public function parseURL(){
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            
+            return $url;
+        }
     }
 }
